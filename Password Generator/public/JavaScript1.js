5bd6ab32685a6e0b68ef3c9937a29aa6bfb18fc2
Trevor Davis <63765564+t-dev7@users.noreply.github.com>
2024-11-22 15:13:22 -0800

    Add files via upload

---
+++ Password Generator/public/JavaScript1.js
@@ -0,0 +1,85 @@
+﻿
+
+function changeFunction (checked) {
+
+    
+    if (checked) { //(TRADITIONAL GENERATION)
+        
+
+        // changes the background color and visibility when slider is checked
+        document.getElementById("flex-trad").style.backgroundColor = "dodgerblue";
+        document.getElementById("flex-dict").style.backgroundColor = "grey";
+        document.getElementById("Dict-body").style.visibility = "hidden";
+        document.getElementById("trad-body").style.visibility = "visible";
+
+        // changes the font size when the slider is checked 
+        document.getElementById("tradTitle").style.fontSize = "50px";
+        document.getElementById("dictTitle").style.fontSize = "40px";
+    }
+    else { // (DICTIONARY GENERATION)
+       
+
+        document.getElementById("flex-trad").style.backgroundColor = "grey";
+        document.getElementById("flex-dict").style.backgroundColor = "red";
+        document.getElementById("Dict-body").style.visibility = "visible";
+        document.getElementById("trad-body").style.visibility = "hidden";
+
+        document.getElementById("tradTitle").style.fontSize = "40px";
+        document.getElementById("dictTitle").style.fontSize = "50px";
+    }
+    
+   
+}// End changeFunction
+
+//////////////////////////////////////////////////////////////////////////
+
+function generate(){
+
+    var checked = document.getElementById("switch").checked;
+
+    // number of words to generate
+    var numWords = document.getElementById("id_selectNumWords").value;
+
+    if(checked){
+        var nodeList = document.querySelectorAll('input[id="cbTrad"]:checked').values();
+        //tradCheckboxes
+        var body = JSON.stringify({
+            value: checked,
+            wordCount: numWords,
+            check: tradCheckboxes
+      
+          });
+    }
+    else{
+        var nodeList = document.querySelectorAll('input[id="cbDict"]:checked');
+        var dictCheckboxes = [];
+
+        nodeList.forEach(node => {
+            dictCheckboxes.push(node.value);
+          });
+
+        
+        var body = JSON.stringify({
+            value: checked,
+            wordCount: numWords,
+            check: dictCheckboxes
+      
+          });
+    }
+
+    // send variables to the server to process
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "/genMethod");
+    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+    
+    xhr.onload = () => {
+      if (xhr.readyState == 4 && xhr.status == 201) {
+        console.log(JSON.parse(xhr.responseText));
+      } else {
+        console.log(`Error: ${xhr.status}`);
+      }
+    };
+    xhr.send(body);
+
+}
+
--- Password Generator/server.js
+++ Password Generator/server.js
@@ -1,22 +1,47 @@
 
+const express = require('express');
 
+const app = express();
+const PORT = 3000;
 var http = require('http');
 const fs = require('fs');
 
 const readline = require('readline');
-var port = process.env.PORT || 1337;
+
 const m_DictWords = new Map();
-var wordCount = 0;
+var csvWordCount = 0;
 
-http.createServer(function (req, res) {
-    fs.readFile('HTMLPage1.html', function (err, data) {
-        
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-    });
-}).listen(port);
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true })); // For form data
 
+app.use(express.json()); // For JSON data
+
+
+app.post('/genMethod', (req, res) => {
+    var checked = req.body.value; // Access the variable
+    var wordCount = req.body.wordCount;
+    var checkBoxes = req.body.check;
+
+    if(!checked)
+        generateDict(wordCount, checkBoxes);
+    else
+        generateTrad(wordCount, checkBoxes);
+
+    console.log(checkBoxes);
+
+  });
+
+
+app.listen(PORT, (error) =>{
+    if(!error){
+        console.log("Server is Successfully Running and App is listening on port "+ PORT);
+        init_DictLoad();
+    }
+        else 
+        console.log("Error occurred, server can't start", error);
+    }
+);
+    
 
 
 
@@ -30,13 +55,12 @@ function init_DictLoad() {
             // loops through the dictionary of words that have a length greater than 5 characters
             for(let line of lines) {
                 if (String(line).length > 5) {
-                    m_DictWords.set(String(line), wordCount);   // maps words to increasing counter numbers
-                    wordCount += 1; // increase the counter
+                    m_DictWords.set(String(csvWordCount), String(line));   // maps words to increasing counter numbers                    
+                    csvWordCount += 1; // increase the counter
 
                     line += ",\r\n";
                     writer.write(String(line));
                     
-
                     const values = line.split(',');
                     
                 }
@@ -47,8 +71,180 @@ function init_DictLoad() {
         } catch (err) {
             console.error(err);
         }
-    
+
 }
 
+function random(int){
+    return (Math.floor(Math.random() * int));
+}
+//////////////////////////////////////////////////////////////////////////
 
-init_DictLoad();
+function generateDict(wordCount, checkBoxes){
+    var gen = "";
+
+    option = "";
+    checkBoxes.forEach((element) => {
+        option += String(element);
+    });
+
+    //! Testing
+    console.log(option);
+  
+   
+            
+    switch (option) {
+        
+        case '1':   // include numbers
+            for(let i = 0; i < wordCount; i++){
+                if(random(2) == 0)
+                    gen+= (String(random(10)) + m_DictWords.get(String(random(csvWordCount))));     // add number at beginning of word
+                else
+                    gen += (m_DictWords.get(String(random(csvWordCount))) + String(random(10)))     // add number at end
+                
+                // Stop the programs from adding the separator "-" at the end 
+                if(i + 1 == wordCount){
+                    // Do Nothing
+                }
+                else{
+                    gen+= ( "-");
+                }
+            }
+            console.log(gen);
+            break;
+
+        case '12':  //include nums and replace common
+            for(let i = 0; i < wordCount; i++){
+                if(random(2) == 0)
+                    gen+= (String(random(10)) + replaceChar(m_DictWords.get(String(random(csvWordCount)))));     // add number at beginning of word
+                else
+                    gen += (replaceChar(m_DictWords.get(String(random(csvWordCount)))) + String(random(10)))     // add number at end
+                
+                // Stop the programs from adding the separator "-" at the end 
+                if(i + 1 == wordCount){
+                    // Do Nothing
+                }
+                else{
+                    gen+= ("-");
+                }
+            }
+            console.log(gen);
+            break;
+
+        case '13':  // include nums and no same starting
+            
+            console.log(option);
+            break;
+
+        case '2':   // Replace common
+            for(let i = 0; i < wordCount; i++){
+                gen += (replaceChar(m_DictWords.get(String(random(csvWordCount)))));
+                    if(i + 1 == wordCount){
+                        // Do Nothing
+                    }
+                    else{
+                        gen+= ("-");
+                    }
+                }
+            console.log(gen);
+            break;
+
+        case '23':   // Replace common and no same starting 
+            console.log(option);
+            break;
+
+            
+        case '3': //  No same starting letter   
+            
+            console.log(option);
+            break;
+        
+        default:
+            for(let i = 0; i < wordCount; i++){
+                gen+= m_DictWords.get(String(random(csvWordCount)));
+
+                if(i + 1 == wordCount){
+                    // Do Nothing
+                }
+                else{
+                    gen+= ("-");
+                }
+            }
+            console.log(gen);
+            break;
+    }
+} 
+ 
+
+function generateTrad(wordCount, checkBoxes){
+    gen = "";
+
+    option = "";
+    checkBoxes.forEach((element) => {
+        option += String(element);
+    });
+
+    console.log(option);
+  
+    switch (option) {
+        
+        case '1':   // include numbers
+            
+            console.log(option);
+            break;
+
+        case '12':  //include nums and replace common
+            
+            console.log(option);
+            break;
+
+        case '13':  // include nums and no same starting
+            
+            console.log(option);
+            break;
+
+        case '2':   // Replace common
+            console.log(option);
+            break;
+
+        case '23':   // Replace common and no same starting 
+            console.log(option);
+            break;
+
+            
+        case '3': //  No same starting letter   
+            
+            console.log(option);
+            break;
+        
+        default:
+            let x = "no";
+            break;
+    }
+}   
+   
+
+function replaceChar(word){
+
+
+    var result = "";
+    for(i = 0; i < word.length; i++)
+
+        switch(word.charAt(i)){
+            case 'a':
+            case 'A':
+                result += '@';
+                break;
+
+            case 'i':
+            case 'I':
+                result += '1';
+                break;
+            
+            
+            
+            default:
+                result += word.charAt(i);
+        }
+
+    return result;
+}
